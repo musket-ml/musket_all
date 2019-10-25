0351f8d4cec75f76ea5ed4a0dbfa5ecb2bcc0c36
Petrochenko Pavel <petrochenko.pavel.a@gmail.com>
2019-10-25 13:17:12 +0700

    Fixing test , to use new coder name

--- musket_ml/examples/modules/datasets.py
+++ musket_ml/examples/modules/datasets.py
@@ -3,7 +3,7 @@ from musket_core import datasets,genericcsv,coders,image_datasets
 @datasets.dataset_provider(origin="train.csv",kind="GenericDataSet")
 def getTitanic():
     return genericcsv.GenericCSVDataSet("train.csv",["Sex","Fare","Age","Pclass"],["Survived"],[],
-                                        {"Sex":"binary","Fare":"normalized_number","Age":"normalized_number","Pclass":"one_hot","Survived":"binary"},input_groups={"0":["Sex","Fare","Age","Pclass"]})
+                                        {"Sex":"binary","Fare":"normalized_number","Age":"normalized_number","Pclass":"multi_class","Survived":"binary"},input_groups={"0":["Sex","Fare","Age","Pclass"]})
     
 @datasets.dataset_provider(origin="saltExists.csv",kind="BinaryClassificationDataSet")
 def getSe():
