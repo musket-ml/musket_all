04b30b7e2a76bba3316982b7c2cd59a6f2c49cd8
Petrochenko Pavel <petrochenko.pavel.a@gmail.com>
2019-11-11 14:13:56 +0700

    updating package version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 import setuptools
 setup(name='musket_ml',
-      version='0.493',
+      version='0.494',
       description='Common parts of my pipelines',
       url='https://github.com/petrochenko-pavel-a/musket_core',
       author='Petrochenko Pavel',
@@ -9,5 +9,5 @@ setup(name='musket_ml',
       license='MIT',
       packages=setuptools.find_packages(),
       include_package_data=True,
-      install_requires=["musket_text>=0.44","musket_core>=0.493","classification_pipeline>=0.431","segmentation_pipeline>=0.431"],
+      install_requires=["musket_text>=0.44","musket_core>=0.494","classification_pipeline>=0.431","segmentation_pipeline>=0.431"],
       zip_safe=False)
