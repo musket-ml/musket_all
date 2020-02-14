ede7cceda19fc725eeead7ccf8f3ee4abffb2b14
Dmitry Karpenko <dmitry@onpositive.com>
2020-02-14 16:52:22 +0700

    Add siamic BERT test
    Change-Id: I5d5c008006b330eab0c8f4bd7e21aa5850870b30

--- musket_ml/examples/modules/datasets.py
+++ musket_ml/examples/modules/datasets.py
@@ -1,4 +1,6 @@
-from musket_core import datasets,genericcsv,coders,image_datasets
+from musket_core import datasets,genericcsv,coders,image_datasets, preprocessing, context
+from numpy import ndarray
+import numpy as np
 
 @datasets.dataset_provider(origin="train.csv",kind="GenericDataSet")
 def getTitanic():
@@ -13,3 +15,25 @@ def getSe():
 @datasets.dataset_provider(origin="train.csv",kind="BinarySegmentationDataSet")
 def getSaltTrain():
     return image_datasets.BinarySegmentationDataSet(["images","images"],"salt.csv","id","rle_mask")    
+
+# We create separate dataset for the siamic network - with 2 outputs
+class Questions2Outputs(datasets.DataSet):
+    
+    def __init__(self):
+        self.data = context.csv_from_data('questions.csv')
+        self.q1 = self.data['question1'].values
+        self.q2 = self.data['question2'].values
+        self.target = self.data['is_duplicate'].values
+        
+    def __len__(self):
+        return len(self.data)
+    
+    def get_questions(self, item):
+        return [str(self.q1[item]),str(self.q2[item])]
+    
+    def __getitem__(self, item):
+        return datasets.PredictionItem(item, self.get_questions(item), np.array([self.target[item]]))
+
+@datasets.dataset_provider(origin="questions.csv",kind="")
+def get_train_siamic():
+    return Questions2Outputs()
--- musket_ml/tests/basic_test.py
+++ musket_ml/tests/basic_test.py
@@ -1,11 +1,21 @@
 import unittest
 from musket_core import projects
 from musket_core import parralel
+from musket_core import  deps_download as downloader
 import os
-
+from builtins import str
 
 fl=__file__
 fl=os.path.dirname(fl)
+
+def basic_test(experiment_name: str):
+    pr=projects.Project(os.path.join(fl,"../examples"))
+    exp=pr.byName(experiment_name)
+    tasks=exp.fit()
+    executor = parralel.get_executor(1, 1)
+    executor.execute(tasks)
+    return exp.result(False,True)
+
 class TestStringMethods(unittest.TestCase):
 
     def test_basic_network(self):
@@ -22,25 +32,21 @@ class TestStringMethods(unittest.TestCase):
         pass
     
     def test_image_classifier(self):
-        
-        pr=projects.Project(os.path.join(fl,"../examples"))
-        exp=pr.byName("t2")
-        tasks=exp.fit()
-        executor = parralel.get_executor(1, 1)
-        executor.execute(tasks)
-        r=exp.result(False,True)
+        r = basic_test("t2")       
         self.assertGreaterEqual(r, 0, "Result should be greater then zero")
         self.assertTrue(isinstance(r,float),"result should be float")
         pass
     
-    def test_image_segmentation(self):
-        
-        pr=projects.Project(os.path.join(fl,"../examples"))
-        exp=pr.byName("t3")
-        tasks=exp.fit()
-        executor = parralel.get_executor(1, 1)
-        executor.execute(tasks)
-        r=exp.result(False,True)
+    def test_image_segmentation(self):        
+        r = basic_test("t3")
+        self.assertGreaterEqual(r, 0, "Result should be greater then zero")
+        self.assertTrue(isinstance(r,float),"result should be float")
+        pass
+    
+    def test_siamic_bert(self):
+        root = os.path.join(fl,"../examples")
+        downloader.download(root, False)
+        r = basic_test("siamic_bert")
         self.assertGreaterEqual(r, 0, "Result should be greater then zero")
         self.assertTrue(isinstance(r,float),"result should be float")
         pass
